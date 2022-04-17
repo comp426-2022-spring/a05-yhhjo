812f3375472ef1aeeccd37fe5c4372fe48156446
yhhjo <ajb28@live.unc.edu>
2022-04-17 13:07:10 -0400

    Outlining basic HTML, buttons, etc.

--- public/main.js
+++ public/main.js
@@ -1,7 +1,32 @@
-// Focus div based on nav button click
+const PATHS = {
+    flip: '/app/flip/',
+    base: 'http://localhost:5555/'
+} 
 
+// Focus div based on nav button click
+function focusDiv(id) {
+    // Turn all active div's off
+    const alldivs = document.getElementsByTagName("div");
+    for (let i = 0; i < alldivs.length; i++) {
+        alldivs[i].setAttribute("class", "hidden");
+    }
+    // Focus div with id = 'id'
+    document.getElementById(id).setAttribute("class", "active");
+}
 // Flip one coin and show coin image to match result when button clicked
 
+function coinFlip() {
+    const response = fetch('http://localhost:5555/app/flip/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (result) {
+        console.log(result);
+        document.getElementById("response").innerHTML = result.flip;
+    })
+    document.getElementById("request").innerHTML = "Requeset: " + response
+
+}
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
 
