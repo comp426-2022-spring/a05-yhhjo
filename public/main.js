a3a1d7901016a7f585d9f18de60942ba11ac09a0
yhhjo <ajb28@live.unc.edu>
2022-04-25 14:23:32 -0400

    removed the morgan dependency

--- public/main.js
+++ public/main.js
@@ -13,21 +13,29 @@ function focusDiv(id) {
     // Focus div with id = 'id'
     document.getElementById(id).setAttribute("class", "active");
 }
-// Flip one coin and show coin image to match result when button clicked
 
+
+// Flip one coin and show coin image to match result when button clicked
 function coinFlip() {
     const response = fetch('http://localhost:5555/app/flip/')
     .then(function (response) {
         return response.json();
     })
     .then(function (result) {
-        console.log(result);
         document.getElementById("response").innerHTML = result.flip;
+        document.getElementById("coinflip").setAttribute("src", "assets/img/" + result.flip + ".png")
     })
-    document.getElementById("request").innerHTML = "Requeset: " + response
-
 }
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
+
+function coinFlips() {
+    const nflips = document.getElementById("nflips")
+}
+
+const form = document.querySelector("#flips");
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
+})
 
 // Guess a flip by clicking either heads or tails button
