1914b1cb06c76cbf8af1579fb9e85e0e10137569
yhhjo <ajb28@live.unc.edu>
2022-04-26 19:15:39 -0400

    generated results table

--- public/main.js
+++ public/main.js
@@ -1,9 +1,9 @@
 const PATHS = {
     flip: '/app/flip/',
     base: 'http://localhost:5555/',
-    flips:'app/flips/'
+    flips: 'app/flip/coins/'
 
-} 
+}
 
 // Focus div based on nav button click
 function focusDiv(id) {
@@ -20,13 +20,13 @@ function focusDiv(id) {
 // Flip one coin and show coin image to match result when button clicked
 function coinFlip() {
     const response = fetch('http://localhost:5555/app/flip/')
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (result) {
-        document.getElementById("response").innerHTML = result.flip;
-        document.getElementById("coinflip").setAttribute("src", "assets/img/" + result.flip + ".png")
-    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (result) {
+            document.getElementById("response").innerHTML = result.flip;
+            document.getElementById("coinflip").setAttribute("src", "assets/img/" + result.flip + ".png")
+        })
 }
 // Flip multiple coins and show coin images in table as well as summary results
 // Enter number and press button to activate coin flip series
@@ -34,12 +34,52 @@ function coinFlip() {
 
 function coinFlips() {
     const nflips = document.getElementById("number").value
-    const req = PATHS.base + PATHS.flips +nflips
-    
+    const api = PATHS.base + PATHS.flips
     // Debug
-    document.getElementById("request").innerHTML = req;
+    document.getElementById("request").innerHTML = "fetch: " + api + nflips;
+
+    fetch(api, {
+        body: JSON.stringify({ "number": nflips }),
+        headers: { "Content-Type": "application/json", },
+        method: "post"
+    }).then((response) => {
+        return response.json();
+    }).then((outcome) => {
+        // Provide a summary
+        document.getElementById("results").setAttribute("class", "active")
+        document.getElementById("headcount").innerHTML = "heads= " + outcome.summary.heads;
+        document.getElementById("tailcount").innerHTML = "tails= " + outcome.summary.tails;
+
+        // Display the results
+        // Remove old results
+        resultsTable("heads", outcome)
+        resultsTable("tails", outcome)
+
+    })
 }
 
-
+// Displays a row of heads or a row of tails
+function resultsTable(ht, outcome) {
+    let hr = document.getElementById(ht)
+    // Remove old results
+    while(hr.firstChild) {
+        hr.removeChild(hr.firstChild)
+    }
+    var tr = document.createElement("tr")
+    let to = ht=="heads" ? outcome.summary.heads : outcome.summary.tails
+    hr.appendChild(tr)
+    for (let i = 0; i < to; i++) {
+        var td = document.createElement("td")
+        var img = document.createElement("img")
+        img.setAttribute("src", "assets/img/" + ht + ".png")
+        img.setAttribute("class", "smallcoin")
+        /*
+        var img = heads.createElement("img")
+        heads.setAttribute("src", "assets/img/heads.png")
+        */
+        td.appendChild(img)
+        tr.appendChild(td)
+    }
+}
 
 // Guess a flip by clicking either heads or tails button
